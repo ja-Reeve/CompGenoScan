67ccb35067ac679abe4dfe64b8af7f2e2f5ef674
ja-Reeve <82411887+ja-Reeve@users.noreply.github.com>
2021-08-25 11:06:37 +0200

    Update Manuscript_Fig2_plot_v2.R
    Minor edits. Tu scores are no longer mapped to the Ts genome and label sizes are now set via a single parameter.

--- Figures/Manuscript_Fig2_plot_v2.R
+++ Figures/Manuscript_Fig2_plot_v2.R
@@ -1,8 +1,8 @@
 ### Script to create Manhattan plots of Fst and He for each species ###
 ### Fst scores are downsamples to every 100th SNP
-### Ninespine SNPs are plotted on the Threespine genome
+### Tubesnout and Ninespine SNPs are plotted on the Threespine genome
 ### James Reeve - University of Gothenburg
-### 15/04/2021
+### 05/04/2021
 
 ### Prepreation
 rm(list = ls())
@@ -13,7 +13,6 @@ setwd("C:/Users/James/Desktop")
 ### Packages
 library("tidyverse")
 library("cowplot")
-library("grid")
 library("gridExtra")
 
 ### A: Access Fst data ####
@@ -44,11 +43,24 @@ Ns.He <- read.table("file:///F:/nucleotide_diversity/Ns_Avg_Hexp_genes.txt", hea
 Ns.He <- Ns.He[Ns.He$GENE != "itgr", ]
 Ns.He$MID_POS <- round(apply(Ns.He[,2:3], 1, median))
 
-### C: Map ninespine scores onto threespine genome ####
+### C: Map all scores onto threespine genome ####
 
 ### Load gene annotations
 Ts.gff <- read.table("file:///F:/GFF/Ts_gene_anno.txt", header = TRUE)
 Ns.gff <- read.table("file:///F:/GFF/Ns_gene_anno.txt", header = TRUE)
+Tu.gff <- read.table("file:///F:/GFF/Tu_gene_anno.txt", header = TRUE)
+
+### Identify gene matches between threespine and tubesnout
+# Ortholog table
+ortho <- read.table("file:///F:/GFF/GAC-AFL.txt", sep = "\t")[,-c(1:2,6)]
+colnames(ortho) <- c("threespine_ID", "tubesnout_ID", "type")
+ortho <- ortho[ortho$type == "1:1",]
+# Replace gene column in Tu.GFF with threespine gene
+Tu.gff.2 <- Tu.gff
+Tu.gff.2$Gene <- gsub("ID=", "AFL_", Tu.gff.2$Gene)
+Tu.gff.2 <- merge(Tu.gff.2, ortho, by.x = "Gene", by.y = "tubesnout_ID")[,2:5]
+colnames(Tu.gff.2) <- c(colnames(Tu.gff.2)[1:3], "Gene")
+Tu.gff.2$Gene <- gsub("GAC_", "ID=", Tu.gff.2$Gene)
 
 ### Function to map species positions onto threepine genome
 # I've set this up so you have the option of running either Fst or He scores with the same function
@@ -105,6 +117,11 @@ Ns.Fst.2 <- Ns.Fst.2[Ns.Fst.2$CHROM_Ts != "chrUn",]
 # Ninespine He
 Ns.He.2 <- Map_to_threespine(Ts.gff, Ns.gff, genetic.score = "He", Ns.He)
 Ns.He.2 <- Ns.He.2[Ns.He.2$CHROM_Ts != "chrUn",]
+# Tubesnout Fst
+# Not used in the final figure, as few SNPs map to threespine chr6
+#Tu.Fst.2 <- Map_to_threespine(Ts.gff, Tu.gff.2, genetic.score = "Fst", Tu.Fst)
+# Tubesnout He
+#Tu.He.2 <- Map_to_threespine(Ts.gff, Tu.gff.2, genetic.score = "He", Tu.He)
 
 ### Convert chromosome names
 # Threespine chr to arabic numerals
@@ -115,13 +132,16 @@ new_chrom_list <- c("chr1", "chr2", "chr3", "chr4", "chr9", "chr5", "chr6", "chr
                        "chr20", "chr21")
 for (i in 1:length(new_chrom_list)) {
   Ts.He[Ts.He$CHROM == chrom_list[i],1] <- new_chrom_list[i]
+  Tu.He.2[Tu.He.2$CHROM_Ts == chrom_list[i], "CHROM_Ts"] <- new_chrom_list[i]
   Ns.He.2[Ns.He.2$CHROM_Ts == chrom_list[i], "CHROM_Ts"] <- new_chrom_list[i]
+  
+  Tu.Fst.2[Tu.Fst.2$CHROM_Ts == chrom_list[i], "CHROM_Ts"] <- new_chrom_list[i]
   Ns.Fst.2[Ns.Fst.2$CHROM_Ts == chrom_list[i], "CHROM_Ts"] <- new_chrom_list[i]
 }; rm(i, chrom_list, new_chrom_list)
 
 # Chage "HiC_chromosome_" to "chr" for Tu
-Tu.He$CHROM <- gsub("HiC_chromosome_", "chr", Tu.He$CHROM)
-Tu.Fst$CHROM <- gsub("HiC_chromosome_", "chr", Tu.Fst$CHROM)
+Tu.He.2$CHROM <- gsub("HiC_chromosome_", "chr", Tu.He.2$CHROM)
+Tu.Fst.2$CHROM <- gsub("HiC_chromosome_", "chr", Tu.Fst.2$CHROM)
 
 ### D: Downsample Fst ####
 ### Downsample Fst scores - this saves on processing power while still showing genetic patterns
@@ -165,11 +185,11 @@ Ts.He.2 <- Ts.He %>% group_by(CHROM_test) %>% summarise(chrom_len=max(MID_POS)) 
   arrange(CHROM_test, MID_POS) %>% mutate(POScumm=MID_POS+tot)
 
 # Tubesnout He
-Tu.He$CHROM_test <- factor(Tu.He$CHROM, levels = paste0("chr",1:23))
+Tu.He.2$CHROM_test <- factor(Tu.He.2$CHROM, levels = paste0("chr",1:23))
 
-Tu.He.2 <- Tu.He %>% group_by(CHROM_test) %>% summarise(chrom_len=max(MID_POS)) %>%
+Tu.He.3 <- Tu.He.2 %>% group_by(CHROM_test) %>% summarise(chrom_len=max(MID_POS)) %>%
   mutate(tot=cumsum(chrom_len)-chrom_len) %>% select(-chrom_len) %>%
-  left_join(Tu.He, ., by = c("CHROM_test"="CHROM_test")) %>%
+  left_join(Tu.He.2, ., by = c("CHROM_test"="CHROM_test")) %>%
   arrange(CHROM_test, MID_POS) %>% mutate(POScumm=MID_POS+tot)
 
 # Ninespine He
@@ -182,6 +202,9 @@ Ns.He.3 <- Ns.He.2 %>% group_by(CHROM_test) %>% summarise(chrom_len=max(MID_Ts))
 
 
 ### E: Manhattan plots ####
+
+txt_size <- 12 # Label text size
+pnt_size <- 5 # Axis tick size
 
 ### Threespine
 # Find centre point of each chromosome
@@ -205,7 +228,7 @@ Ts.Manhat.Fst <- ggplot(Ts.Fst.3, aes(x=POScumm, y=Fst)) +
   
   # Customize the theme:
   theme(axis.title = element_blank(),
-        axis.text = element_text(size = 8),
+        axis.text = element_text(size = txt_size),
         panel.grid.minor = element_blank(),
         panel.background = element_rect(fill = "white"),
         axis.line = element_line(),
@@ -219,7 +242,7 @@ Ts.Manhat.He <- ggplot(Ts.He.2, aes(x=POScumm, y=Avg_PI)) +
   scale_y_continuous(expand = c(0, 0), limits = c(0,0.02), breaks=seq(0,0.02,0.01)) +     # remove space between plot area and x axis
   labs(title = "Threespine Stickleback (Ts)")+
   theme(axis.title = element_blank(),
-        axis.text = element_text(size = 8),
+        axis.text = element_text(size = txt_size),
         panel.grid.minor = element_blank(),
         panel.background = element_rect(fill = "white"),
         axis.line = element_line(),
@@ -229,31 +252,29 @@ Ts.Manhat.He <- ggplot(Ts.He.2, aes(x=POScumm, y=Avg_PI)) +
 
 
 ### Tubesnout
-Tu.tmp <- Tu.Fst.3 %>% group_by(CHROM) %>% summarize(center=( max(POScumm) + min(POScumm) ) / 2 )
-
 Tu.Manhat.Fst <- ggplot(Tu.Fst.3, aes(x=POScumm, y=Fst)) +
   geom_hline(aes(yintercept = quantile(Tu.Fst$Fst, 0.999)), colour = "red", lty = 2)+
   geom_point( aes(color=CHROM_test), alpha=0.1, size=0.01) +
   scale_color_manual(values = rep(c("navy", "orange"), 12 ))+
-  scale_x_continuous( label = sapply(strsplit(Tu.tmp$CHROM, "r"), `[[`, 2), breaks = Tu.tmp$center ) +
+  scale_x_continuous( label = sapply(strsplit(Ts.tmp$CHROM, "r"), `[[`, 2), breaks = Ts.tmp$center ) +
   scale_y_continuous(expand = c(0, 0), limits = c(0,1), breaks=seq(0,1,0.5)) +
   labs(title = "Tubesnout (Tu)")+
   theme(axis.title = element_blank(),
-        axis.text = element_text(size = 8),
+        axis.text = element_text(size = txt_size),
         panel.grid.minor = element_blank(),
         panel.background = element_rect(fill = "white"),
         axis.line = element_line(),
         legend.position = "None")
 
-Tu.Manhat.He <- ggplot(Tu.He.2, aes(x=POScumm, y=Avg_PI)) +
+Tu.Manhat.He <- ggplot(Tu.He.3, aes(x=POScumm, y=Avg_PI)) +
   geom_hline(yintercept = quantile(Tu.He$Avg_PI, 0.99), colour = "red", lty = 2)+
   geom_point( aes(color=CHROM_test), alpha=0.1, size=0.01) +
   scale_color_manual(values = rep(c("purple", "black"), 12 ))+
-  scale_x_continuous( label = sapply(strsplit(Tu.tmp$CHROM, "r"), `[[`, 2), breaks = Tu.tmp$center ) +
+  scale_x_continuous( label = sapply(strsplit(Ts.tmp$CHROM, "r"), `[[`, 2), breaks = Ts.tmp$center ) +
   scale_y_continuous(expand = c(0, 0), limits = c(0,0.02), breaks=seq(0,0.02,0.01)) +     # remove space between plot area and x axis
   labs(title = "Tubesnout (Tu)")+
   theme(axis.title = element_blank(),
-        axis.text = element_text(size = 8),
+        axis.text = element_text(size = txt_size),
         panel.grid.minor = element_blank(),
         panel.background = element_rect(fill = "white"),
         axis.line = element_line(),
@@ -273,7 +294,7 @@ Ns.Manhat.Fst <- ggplot(Ns.Fst.3, aes(x=POScumm, y=Avg_Fst))+
   scale_y_continuous(expand = c(0, 0), limits = c(0,1), breaks=seq(0,1,0.5))+
   labs(title = "Ninespine Stickleback (Ns)")+
   theme(axis.title = element_blank(),
-        axis.text = element_text(size = 8),
+        axis.text = element_text(size = txt_size),
         panel.grid.minor = element_blank(),
         panel.background = element_rect(fill = "white"),
         axis.line = element_line(),
@@ -287,7 +308,7 @@ Ns.Manhat.He <- ggplot(Ns.He.3, aes(x=POScumm, y=Avg_PI)) +
   scale_y_continuous(expand = c(0, 0), limits = c(0,0.02), breaks=seq(0,02,0.01)) +     # remove space between plot area and x axis
   labs(title = "Ninespine Stickleback (Ns)")+
   theme(axis.title = element_blank(),
-        axis.text = element_text(size = 8),
+        axis.text = element_text(size = txt_size),
         panel.grid.minor = element_blank(),
         panel.background = element_rect(fill = "white"),
         axis.line = element_line(),
@@ -296,12 +317,17 @@ Ns.Manhat.He <- ggplot(Ns.He.3, aes(x=POScumm, y=Avg_PI)) +
 # 57 genes He > 0.02
 
 ### Multi-panel plot
-labsY <- textGrob(expression(F[ST]), rot = 90, gp = gpar(fontsize = 20))
+labsY <- textGrob(expression(F[ST]), rot = 90, gp = gpar(fontsize = txt_size+2))
 p1 <- grid.arrange(Ts.Manhat.Fst, Ns.Manhat.Fst, Tu.Manhat.Fst, ncol = 1, left = labsY)
 
-labsY <- textGrob(expression(bar(H[E])), rot = 90, gp = gpar(fontsize = 20))
-labsX <- textGrob("Position", gp = gpar(fontsize = 18))
+labsY <- textGrob(expression(bar(H[E])), rot = 90, gp = gpar(fontsize = txt_size+2))
+labsX <- textGrob("Position", gp = gpar(fontsize = txt_size))
 p2 <- grid.arrange(Ts.Manhat.He, Ns.Manhat.He, Tu.Manhat.He, ncol = 1, left = labsY, bottom = labsX)
 
-Fig2 <- plot_grid(p1, p2, labels = "AUTO", label_size = 24, ncol = 1)
+Fig2 <- plot_grid(p1, p2, labels = "AUTO", label_size = txt_size*2, ncol = 1)
 
+# Save plot
+tiff("/Users/james/Dropbox (Personal)/Comp_geno_2020/Figure_2.tiff", width = 18, height = 16,
+     units = "cm", res = 200)
+grid.draw(Fig2)
+dev.off()
