7393ac5fdc96fed0e054b8bc3bb9ed3f96117d5d
ja-Reeve <82411887+ja-Reeve@users.noreply.github.com>
2021-08-26 16:30:14 +0200

    Update Ts_Tu_50Kwind.R
    Major change: using top candidate genes instead of top candidate windows to find overlapping signatures of selection!
    I also cleaned up the code for the gene.assign functions.

--- R_scripts/species_comparisons/Ts_Tu_50Kwind.R
+++ R_scripts/species_comparisons/Ts_Tu_50Kwind.R
@@ -1,6 +1,7 @@
 #### Testing comparisons among Ts.He and Tu.He using 50Kb windows ####
 ### James Reeve
 ### 19/09/2019
+### Edit: replace candiates detected in 50Kb window with candidates per gene (26/08/2021)
 
 rm(list = ls())
 dev.off()
@@ -10,62 +11,41 @@ options(stringsAsFactors = FALSE)
 library(ggplot2)
 
 ### Download He data
-Ts.He <- read.table("file:///G:/nucleotide_diversity/Ts_Avg_Hexp_50K.txt", header = TRUE)
-Tu.He <- read.table("file:///G:/nucleotide_diversity/Tu_Avg_Hexp_50K.txt", header = TRUE)
+Ts.He <- read.table("file:///F:/nucleotide_diversity/Ts_Avg_Hexp_50Kwind.txt", header = TRUE)
+Tu.He <- read.table("file:///F:/nucleotide_diversity/Tu_Avg_Hexp_50Kwind.txt", header = TRUE)
 
 Ts.He <- Ts.He[complete.cases(Ts.He),]
 Tu.He <- Tu.He[complete.cases(Tu.He),]
 
 ### Download Fst data
-Ts.Fst <- read.table("file:///G:/Fst-table/Ts.Fst.50K.txt", header = TRUE)
-Tu.Fst <- read.table("file:///G:/Fst-table/Tu.Fst.50K.txt", header = TRUE)
-# assign top candidate flag
-Ts.TC <- read.table("file:///G:/top-candidate/Ts.TopCandidates.50Kwind.txt", header = TRUE)
+Ts.Fst <- read.table("file:///F:/Fst-table/Ts.Fst.50K.txt", header = TRUE)
+Tu.Fst <- read.table("file:///F:/Fst-table/Tu.Fst.50K.txt", header = TRUE)
+
+### Download top canadidate data
+Ts.TC <- read.table("file:///F:/top-candidate/Ts.VarScan.TopCandidates.txt", header = TRUE)
+Tu.TC <- read.table("file:///F:/top-candidate/Tu.VarScan.TopCandidates.txt", header = TRUE)
+# Remove inter-geneic regions
+Ts.TC <- Ts.TC[Ts.TC$Gene_Name != 'itgr',]
+Tu.TC <- Tu.TC[Tu.TC$Gene_Name != 'itgr',]
+# Create flag for top candidates
 Ts.TC$CAND <- Ts.TC$Observed_Outliers > Ts.TC$Expected_Outliers
-Tu.TC <- read.table("file:///G:/top-candidate/Tu.TopCandidates.50Kwind.txt", header = TRUE)
 Tu.TC$CAND <- Tu.TC$Observed_Outliers > Tu.TC$Expected_Outliers
-
-Ts.Fst <- merge(Ts.Fst, Ts.TC, by.x = "WIND", by.y = "Window_Name")[,c(2:5,12,1)] 
-Tu.Fst <- merge(Tu.Fst, Tu.TC, by.x = "WIND", by.y = "Window_Name")[,c(2:5,12,1)] 
-
-rm(Ts.TC, Tu.TC)
-
-### Download gene annotations
-Ts.gff <- read.table("file:///G:/GFF/Ts_gene_anno.txt", header = TRUE)
-Ts.gff <- Ts.gff[Ts.gff$CHROM != "chrUn", ]
-Tu.gff <- read.table("file:///G:/GFF/Tu_gene_anno.txt", header = TRUE)
-# Rename gene header
-Ts.gff$Gene <- paste("GAC", sapply(strsplit(Ts.gff$Gene, "[=]"), `[[`, 2), sep = "_")
-Tu.gff$Gene <- paste("AFL", sapply(strsplit(Tu.gff$Gene, "[=]"), `[[`, 2), sep = "_")
+# Rename gene prefix
+Ts.TC$Gene_Name <- gsub("ID=", "GAC_", Ts.TC$Gene_Name)
+Tu.TC$Gene_Name <- gsub("ID=", "AFL_", Tu.TC$Gene_Name)
 
 ### Download ortholog table
-ortho <- read.table("file:///G:/GFF/GAC-AFL.txt", sep = "\t")[,-c(1:2,6)]
+ortho <- read.table("file:///F:/GFF/GAC-AFL.txt", sep = "\t")[,-c(1:2,6)]
 colnames(ortho) <- c("threespine_ID", "tubesnout_ID", "type")
 ortho <- ortho[ortho$type == "1:1",]
 
-### Update chromosome names
-chrom_list_Ts <- read.table("file:///G:/chrom_list/Ts_chr_list.txt")$V1[-c(6:7)]
-new_chrom_list_Ts <- c("chr1", "chr2", "chr3", "chr4", "chr9", "chr5", "chr6", "chr7", "chr8",
-                    "chr10", "chr11", "chr12", "chr13", "chr14", "chr19", "chr15", "chr16", "chr17",
-                    "chr18", "chr20", "chr21")
-for(i in 1:length(chrom_list_Ts)){
-  Ts.gff[Ts.gff$CHROM == chrom_list_Ts[i], 1] <- new_chrom_list_Ts[i]
-}; rm(i, chrom_list_Ts, new_chrom_list_Ts)
-
-chrom_list_Tu <- read.table("file:///G:/chrom_list/Tu_chr_list.txt")$V1
-new_chrom_list_Tu <- paste0("chr",1:23)
-for(i in 1:length(chrom_list_Tu)){
-  Tu.gff[Tu.gff$CHROM == chrom_list_Tu[i], 1] <- new_chrom_list_Tu[i]
-}; rm(i, chrom_list_Tu, new_chrom_list_Tu)
-
-
 ### Merge He and Fst data
-Ts.Fst.He <- merge(Ts.He, Ts.Fst, by = "WIND")[,c(2:7,11:12,1)]
+Ts.Fst.He <- merge(Ts.He, Ts.Fst, by = "WIND")[,c(2:7,11,1)]
 colnames(Ts.Fst.He) <- c("CHROM", "START_POS", "END_POS", "TsAK_He", "TsOR_He",
-                         "Avg_He", "Avg_Fst", "CAND", "WINDOW")
-Tu.Fst.He <- merge(Tu.He, Tu.Fst, by = "WIND")[,c(2:7,11:12,1)]
+                         "Avg_He", "Avg_Fst", "WINDOW")
+Tu.Fst.He <- merge(Tu.He, Tu.Fst, by = "WIND")[,c(2:7,11,1)]
 colnames(Tu.Fst.He) <- c("CHROM", "START_POS", "END_POS", "TuAK_He", "TuBC_He",
-                         "Avg_He", "Avg_Fst", "CAND", "WINDOW")
+                         "Avg_He", "Avg_Fst", "WINDOW")
 
 ### Assing window He to genes within windows ####
 
@@ -77,18 +57,21 @@ GeneAsign <- function(data, gene.annotation){
     Spos <- data[i, "START_POS"]
     Epos <- data[i, "END_POS"]
     
-    dat <- gene.annotation[gene.annotation$CHROM == Chrom &
-                             gene.annotation$Start_Pos >= Spos &
-                             gene.annotation$End_Pos <= Epos, ]
+    dat <- gene.annotation[gene.annotation$Chromosome == Chrom &
+                             gene.annotation$Gene_Start >= Spos &
+                             gene.annotation$Gene_End <= Epos, ]
     if(nrow(dat) == 0)next
-    dat$North_He <- rep(data[i, 4], nrow(dat))
-    dat$South_He <- rep(data[i, 5], nrow(dat))
-    dat$Avg_He <- rep(data[i, "Avg_He"], nrow(dat))
-    dat$Avg_Fst <- rep(data[i, "Avg_Fst"], nrow(dat))
-    dat$CAND <- rep(data[i, "CAND"], nrow(dat))
-    dat$WINDOW <- rep(data[i, "WINDOW"], nrow(dat))
-    
-    tmp[[i]] <- dat
+
+    tmp[[i]] <- data.frame("CHROM" = Chrom, 
+                           "Start_Pos" = Spos, 
+                           "End_Pos" = Epos, 
+                           "Gene" = dat$Gene_Name[i],
+                           "North_He" = data[i, 4],
+                           "South_He" = data[i, 5],
+                           "Avg_He" = data[i, "Avg_He"],
+                           "Avg_Fst" = data[i, "Avg_Fst"],
+                           "CAND" = dat$CAND[i],
+                           "WINDOW" = data[i,"WINDOW"])
     
     rm(Chrom, Spos, Epos, dat)
   }; rm(i)
@@ -98,23 +81,23 @@ GeneAsign <- function(data, gene.annotation){
   return(tmp2)
 }
 
-Ts.gene <- GeneAsign(Ts.Fst.He, Ts.gff)
-Tu.gene <- GeneAsign(Tu.Fst.He, Tu.gff)
+Ts.gene <- GeneAsign(Ts.Fst.He, Ts.TC)
+Tu.gene <- GeneAsign(Tu.Fst.He, Tu.TC)
 
 ### Assign genes in between windows to window with longer section of gene
 
-GeneAsign2 <- function(data, gene.annotation, assinged.genes){
+GeneAsign2 <- function(data, gene.annotation, assigned.genes){
   # Find genes between windows
-  btw <- gene.annotation[!(gene.annotation[,"Gene"] %in% assinged.genes[,"Gene"]),]
+  btw <- gene.annotation[!(gene.annotation[,"Gene_Name"] %in% assigned.genes[,"Gene"]),]
   
   # Empty dataset
   tmp <- list()
   
   # For loop assinging candidate info to 'btw'
   for(i in 1:nrow(btw)){
-    Chrom <- btw[i, "CHROM"]
-    Spos <- btw[i, "Start_Pos"]
-    Epos <- btw[i, "End_Pos"]
+    Chrom <- btw[i, "Chromosome"]
+    Spos <- btw[i, "Gene_Start"]
+    Epos <- btw[i, "Gene_End"]
     
     Pos <- Spos:Epos
     
@@ -131,32 +114,32 @@ GeneAsign2 <- function(data, gene.annotation, assinged.genes){
       tmp[[i]] <- data.frame("CHROM" = Chrom, 
                              "Start_Pos" = Spos, 
                              "End_Pos" = Epos, 
-                             "Gene" = btw$Gene[i],
+                             "Gene" = btw$Gene_Name[i],
                              "North_He" = dat[1,4],
                              "South_He" = dat[1,5],
                              "Avg_He" = dat[1, "Avg_He"],
                              "Avg_Fst" = dat[1, "Avg_Fst"],
-                             "CAND" = dat[1, "CAND"],
+                             "CAND" = btw$CAND[i],
                              "WINDOW" = dat[1,"WINDOW"])
     } else {
       tmp[[i]] <- data.frame("CHROM" = Chrom, 
                              "Start_Pos" = Spos, 
                              "End_Pos" = Epos, 
-                             "Gene" = btw$Gene[i],
+                             "Gene" = btw$Gene_Name[i],
                              "North_He" = dat[2,4],
                              "South_He" = dat[2,5],
                              "Avg_He" = dat[2, "Avg_He"],
                              "Avg_Fst" = dat[2, "Avg_Fst"],
-                             "CAND" = dat[2, "CAND"],
+                             "CAND" = btw$CAND[i],
                              "WINDOW" = dat[2,"WINDOW"])
     }
     rm(Chrom, Spos, Epos, Pos, dat, wind1, wind2)
   }; rm(i)
   
   tmp2 <- do.call(rbind.data.frame, tmp)
-  
+ 
   # Join genes to end of previously assinged genes
-  tmp3 <- rbind(assinged.genes, tmp2)
+  tmp3 <- rbind(assigned.genes, tmp2)
   
   # sort
   tmp4 <- tmp3[with(tmp3, order(CHROM, Start_Pos)), ]
@@ -164,17 +147,19 @@ GeneAsign2 <- function(data, gene.annotation, assinged.genes){
   return(tmp4) 
 }
 
-Ts.gene2 <- GeneAsign2(Ts.Fst.He, Ts.gff, Ts.gene)
-Tu.gene2 <- GeneAsign2(Tu.Fst.He, Tu.gff, Tu.gene)
+Ts.gene2 <- GeneAsign2(Ts.Fst.He, Ts.TC, Ts.gene)
+Tu.gene2 <- GeneAsign2(Tu.Fst.He, Tu.TC, Tu.gene)
 
 ###3. Merge Threespine and tubesnouts
 tmp1 <- merge(Ts.gene2, ortho, by.x = "Gene", by.y = "threespine_ID")
 tmp2 <- merge(tmp1, Tu.gene2, by.x = "tubesnout_ID", by.y = "Gene")
-Ts_Tu <- tmp2[, c(3:11,2,13:21,1)]
-colnames(Ts_Tu) <- c("CHROM.Ts", "Start_Pos.Ts", "End_Pos.Ts", "TsAK_He", "TsOR_He",
-                    "Avg_He.Ts", "Avg_Fst.Ts", "CAND.Ts", "Window.Ts", "Gene.Ts",
-                     "CHROM.Tu", "Start_Pos.Tu", "End_Pos.Tu", "TuAK_He", "TuBC_He",
-                     "Avg_He.Tu", "Avg_Fst.Tu", "CAND.Tu", "Window.Tu", "Gene.Tu")
+Ts_Tu <- tmp2[, c(3:9, 11, 2, 10, 13:19, 21, 1, 20)]
+colnames(Ts_Tu) <- c("CHROM.Ts", "Start_Pos.Ts", "End_Pos.Ts",
+                     "TsAK_He", "TsOR_He","Avg_He.Ts", "Avg_Fst.Ts",  
+                     "Window.Ts", "Gene.Ts", "CAND.Ts",
+                     "CHROM.Tu", "Start_Pos.Tu", "End_Pos.Tu", 
+                     "TuAK_He", "TuBC_He", "Avg_He.Tu", "Avg_Fst.Tu", 
+                     "Window.Tu", "Gene.Tu", "CAND.Tu")
 rm(tmp1, tmp2)
 
 ###4. Find outluiers 
@@ -267,5 +252,5 @@ ggplot(Ts_Tu)+
 
 
 ###7. Save data
-write.table(Ts_Tu, "file:///G:/Ts_Tu_50Kdata.txt")
+write.table(Ts_Tu, "file:///F:/Ts_Tu_50Kdata.txt")
 
